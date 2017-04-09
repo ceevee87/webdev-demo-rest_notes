002cd3f0764ce012a73f6050b7a91d9b70796bef
John Ramirez <cyclopsvision87@icloud.com>
2017-04-08 18:32:20 -0600

    Implement in-place deletion of a single task. Current solution is a table to list tasks and their IDs. I add in an AJAX method to remove a task item from the web server plus update the table on the HTML side.
    There is commented out code/unused code that adds in new tasks as list items.
    I'm not sure if I'll go back to this solution or not. For now, I leave it
    in but commented out.

--- Web/public/assets/js/ajax.js
+++ Web/public/assets/js/ajax.js
@@ -5,12 +5,47 @@ function createNewListItemHTML(task) {
     "<li class=\"list-group-item\"> \
         <span class=\"lead\">" + task + "</span> \
         <div class=\"pull-right\"> \
-            <button class=\"btn btn-sm btn-warning edit-button\">Edit</button> \
-            <button class=\"btn btn-sm btn-danger edit-button\">Delete</button> \
+            <button class=\"btn btn-sm btn-warning edit-task-button\">Edit</button> \
+            <button class=\"btn btn-sm btn-danger del-task-button\">Delete</button> \
         </div> \
     </li>";
     return htmlStr;
 }
+
+function createNewRowItemHTML(task) {
+    var htmlStr = 
+        "<tr class=\"task-row\">\
+            <th scope=\"row\">"+task.id+"</th>\
+            <td>"+task.body+"</td>\
+            <td style=\"width:20%\">\
+                <div class=\"pull-right\"> \
+                    <button class=\"btn btn-sm btn-warning edit-task-button\">Edit</button> \
+                    <button class=\"btn btn-sm btn-danger del-task-button\">Delete</button> \
+                </div> \
+            </td>\
+        </tr>"
+    return htmlStr;
+}
+
+$('.table').on('click','.del-task-button', function(event) {
+    var taskRowObj = $(this).parents('tr')
+    var taskIdCell = taskRowObj.find('th');
+    var url = 'http://localhost:8080/Note/api/notes/' + taskIdCell.text(); 
+    $.ajax({ 
+        url: url, 
+        type: 'DELETE', 
+        success: function(data) { 
+            console.log(JSON.stringify(data,null,'\t'))
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) { 
+            console.log("AJAX DELETE didn't work!!!:" + 
+                    JSON.stringify(XMLHttpRequest,null,'\t'));
+            console.log("Status: " + textStatus); 
+            console.log("Error: " + errorThrown); 
+        } 
+    });
+    taskRowObj.remove();
+});
 
 $('#notetaker_get').click(function(event){
     console.log("Clicked the button.");
@@ -19,9 +54,10 @@ $('#notetaker_get').click(function(event){
         type: 'GET', 
         success: function(data) { 
             console.log(JSON.stringify(data,null,'\t')) 
-            $('.note-list-group').empty();
+            $('table.table').find('.task-row').remove();
             data.forEach(function(task){
-                $('.note-list-group').append(createNewListItemHTML(task.body));
+                $('table.table').append(createNewRowItemHTML(task));            
+                // $('.note-list-group').append(createNewListItemHTML(task.body));
             });
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) { 
@@ -70,8 +106,8 @@ $('#newitemform').submit(function(event){
             console.log(JSON.stringify(data,null,'\t')) 
             console.log("status:" + status); 
             console.log(JSON.stringify(xhr,null,'\t'));
-            $('.note-list-group').append(createNewListItemHTML(data.body));
-            // $('.note-list-group').append("<li>"+data.body+"</li>");
+            $('table.table').append(createNewRowItemHTML(data));
+            // $('.note-list-group').append(createNewListItemHTML(data.body));
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) { 
             console.log("AJAX POST didn't work!!! " + 
